dccdaed2ff84fea5ae97c8acba85dba2c9c56df7
swojeet <swojeetkayastha@gmail.com>
2019-07-17 15:10:04 +0545

    Update OrderMailer Test

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -2,18 +2,18 @@ require 'test_helper'
 
 class OrderMailerTest < ActionMailer::TestCase
   test "received" do
-    mail = OrderMailer.received
-    assert_equal "Received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderMailer.received(orders(:one))
+    assert_equal "Pramatic Store Order Confirmation", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /1 x Programming Ruby 1.9/, mail.body.encoded
   end
 
   test "shipped" do
-    mail = OrderMailer.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
+    mail = OrderMailer.shipped(orders(:one))
+    assert_equal "Pragmatic Store Order Shipped", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
     assert_match "Hi", mail.body.encoded
   end
 
